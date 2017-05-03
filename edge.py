04c641b2b6a0c599f575706c04e0f06d323fc17a
Hertafeld <ehertafe@oberlin.edu>
2017-05-03 11:40:59 -0400

    crop2

--- edge.py
+++ edge.py
@@ -76,7 +76,7 @@ demo = False
 
 downsample = 1
 
-crop_coords = [850, 1550, 0, 900]
+crop_coords = [1150, 1750, 150, 950]
 
 def process(image_in, lines_out, interactive, params):
 	global rho, theta, threshold, maxLineGap, minLineLength, img, thickness, thickness_canny, PI, blurSize, blurSize_min, blurSize_max, canny, hough, lines
